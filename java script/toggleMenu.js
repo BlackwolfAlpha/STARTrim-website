01c89aff113fe693e52b18bd6dd00c1c5498f8df
BlackwolfAlpha <168213304+BlackwolfAlpha@users.noreply.github.com>
2024-10-03 14:29:35 +0300

    Update toggleMenu.js

--- java script/toggleMenu.js
+++ java script/toggleMenu.js
@@ -1,15 +1,8 @@
 function toggleMenu() {
     const sideMenu = document.getElementById("side-menu");
     if (sideMenu.style.width === "0px" || sideMenu.style.width === "") {
-        sideMenu.style.width = "250px";
+        sideMenu.style.width = "330px";
     } else {
         sideMenu.style.width = "0";
     }
 }
-
-window.addEventListener('resize', function () {
-    const sideMenu = document.getElementById("side-menu");
-    if (window.innerWidth >= 768) {
-        sideMenu.style.width = "0";
-    }
-});
