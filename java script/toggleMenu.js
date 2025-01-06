eb99870a29df67cdff80f02015b799ad58f60cea
BlackwolfAlpha <168213304+BlackwolfAlpha@users.noreply.github.com>
2025-01-06 18:00:37 +0200

    Delete java script directory

--- java script/toggleMenu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const sideMenu = document.getElementById("side-menu");
-    if (sideMenu.style.width === "0px" || sideMenu.style.width === "") {
-        sideMenu.style.width = "330px";
-    } else {
-        sideMenu.style.width = "0";
-    }
-}
